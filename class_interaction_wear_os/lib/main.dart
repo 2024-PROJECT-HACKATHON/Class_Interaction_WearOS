447eea9b09641b252070005e84c8ebbd04f3cbb4
KimSeoWon <ksw1912@gmail.com>
2024-08-01 22:52:59 +0900

    ui 틀만

--- class_interaction_wear_os/lib/main.dart
+++ class_interaction_wear_os/lib/main.dart
@@ -4,16 +4,47 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        visualDensity: VisualDensity.compact,
+        scaffoldBackgroundColor: Colors.black,
+      ),
       home: Scaffold(
-        appBar: AppBar(),
+        body: ListView.separated(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.grey[900],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              elevation: 5,
+              child: ListTile(
+                title: Center(
+                  child: Text(
+                    "Item fsdafdsafdasfasdfsdfsd$index",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider();
+          },
+        ),
       ),
     );
   }
-} 
+}
